43ea862b244ac1513af4de42de22394287fbb366
faisalahammed044274 <setu2014@gmail.com>
2022-08-21 23:14:20 +0600

    Method

---
+++ 03.method/person.php
@@ -0,0 +1,21 @@
+<?php
+
+class Person{
+    //Properties
+    //.................
+
+    //Methods
+    function greeting(){
+        echo "Hello All";
+    }
+
+    function walk(){
+        echo "I can walk";
+    }
+    function talk(){
+        echo "I can talk";
+    }
+    function smell(){
+        echo "I can smell the smoke!";
+    }
+}
---
+++ 03.method/phone.php
@@ -0,0 +1,18 @@
+<?php
+class Phone{
+    //Properties
+    //..........
+
+    //Methods
+    function sendMessage(){
+        echo "Write your Message and press the button to send it";
+    }
+
+    function makeCall(){
+        echo "i can make a call if i dial up a number";
+    }
+
+    function connectToInternet(){
+        echo "I can connect to Internet!";
+    }
+}
