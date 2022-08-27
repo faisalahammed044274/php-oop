c91bdc574756b426f29daadec53dc27f28cea4b8
faisalahammed044274 <setu2014@gmail.com>
2022-08-27 12:25:58 +0600

    construct with optional parameters

--- 07.consructor-destructor/Tree.php
+++ 07.consructor-destructor/Tree.php
@@ -1,24 +1,31 @@
 <?php
 
-class Tree{
+class Tree
+{
 
     //Properties
     public $leavesColor;
     public $numberOfLeaves;
 
     //Construct
-    public function __construct($leavesColor, $numberOfLeaves){
+    public function __construct($leavesColor = "", $numberOfLeaves = "")
+    {
         $this->leavesColor = $leavesColor;
         $this->numberOfLeaves = $numberOfLeaves;
     }
 
-    public function TreeDetails(){
-        echo "This tree have {$this->leavesColor}  Leaves and number of leaves is {$this->numberOfLeaves}.";
+    public function describeTree()
+    {
+        echo "This tree have {$this->numberOfLeaves} {$this->leavesColor} leaves.";
     }
 }
 
-$Tree1 = new Tree('green','Seven');
-$Tree1->TreeDetails();
-
-
-?>
+// case 1 : With Parameters
+$tree1 = new Tree('Green', '45');
+$tree1->describeTree();
+echo PHP_EOL;
+// case 2 optional parameters
+$tree2 = new Tree();
+$tree2->leavesColor = "Grey";
+$tree2->numberOfLeaves = 6577;
+$tree2->describeTree();
