2c01aaabf2db41ffa0b4e97ee1bedfad49329349
faisalahammed044274 <setu2014@gmail.com>
2022-08-30 13:33:52 +0600

    ABSTRACT INFO

---
+++ 14.Abstruct Method and Classes/ABSTRACT-INFO.php
@@ -0,0 +1,20 @@
+<?php
+
+// Abstract class is a class from which we do not want to create an object. We only want to access its methods.
+
+// A class is called abstract when it has at least one abstract method.
+
+// Abstract methods must be defined in the child class.
+
+// We can not create an instance of an abstract class
+
+
+// Interface are similar to abstract classes. The difference between interfaces and abstract classes are:
+
+// 1) Interfaces cannot have properties, while abstract classes can
+
+// 2) All interface methods must be public, while abstract class methods is public or protected
+
+// 3) All methods in an interface are abstract, so they cannot be implemented in code and the abstract keyword is not necessary
+
+// 4) Classes can implement an interface while inheriting from another class at the same time
